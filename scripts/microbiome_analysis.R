077483dd4a314117fdcaddccc7ea7e710b9b880b
AMahajna <asala.mahajna@gmail.com>
2024-08-30 15:28:52 +0200

    reorganize

--- scripts/microbiome_analysis.R
+++ scripts/microbiome_analysis.R
@@ -41,6 +41,9 @@ install.packages("data.table")
 
 # Load the data.table package
 library(data.table)
+
+# Load the dplyr package
+library(dplyr)
 ################################################################################
 
 biom_data = biomformat::read_biom("input_data/Galaxy1760-[Bracken-biom_output_file_(including_metadata)].biom1")
@@ -76,7 +79,11 @@ colData(tse) <- col_data_new
 ##Create bar plots
 
 ################################################################################
+################################################################################
 ##Read samsa2 output 
+
+################################################################################
+##organisms: reading and cleaning 
 
 # Define the path to your folder containing TSV files
 folder_path_org <- "input_data/org_result"
@@ -126,6 +133,87 @@ fwrite(merged_data_org, "output_data/merged_data_org.tsv", sep = "\t")
 # Display the first few rows of the merged data
 head(merged_data_org)
 
+# Remove columns that start with "rel"
+merged_data_org <- merged_data_org %>%
+  select(-starts_with("rel"))
+
+# Remove the prefix "reads_" from column names
+names(merged_data_org) <- gsub("^reads_", "", names(merged_data_org))
+
+# Save the merged data to a new file (optional)
+fwrite(merged_data_org, "output_data/merged_data_org_reads.tsv", sep = "\t")
+
+################################################################################
+##metabolism: reading and cleaning 
+
+#FIX:
 
 
+
+# Define the path to your folder containing TSV files
+folder_path_metabolism <- "input_data/subsystems_result"
+
+# List all TSV files in the folder
+file_list_metabolism <- list.files(folder_path_metabolism, pattern = "\\.reduced$", full.names = TRUE)
+
+# Read the file with comment.char argument (assuming no comment character in your case)
+example <- fread("input_data/subsystems_result/SRR29355722.reduced", header = FALSE, sep = "\t", fill = TRUE)
+
+# Function to read and preprocess each file
+read_and_preprocess <- function(file) {
+  # Extract the file name without extension
+  file_name <- tools::file_path_sans_ext(basename(file))
+  
+  # Read the TSV file
+  df <- fread(file, header = FALSE, sep = "\t")
+  
+  # Rename columns
+  colnames(df) <- c(paste0("rel_abundance_", file_name),
+                    paste0("reads_", file_name),
+                    "enzyme",
+                    "SEED_subsystem",
+                    "SEED_subsystem_functional_category", 
+                    "description")
+  
+  # Ensure 'protein' column is character type for consistency
+  df[, protein := as.character(protein)]
+  
+  # Return the data.table
+  return(df)
+}
+
+# Read and preprocess all files
+data_list_metabolism <- lapply(file_list_metabolism, read_and_preprocess)
+
+# Merge all data frames by taxonomy column
+merged_data_metabolism <- Reduce(function(x, y) merge(x, y, by = c("enzyme",
+                    "SEED_subsystem",
+                    "SEED_subsystem_functional_category", 
+                    "description"), all = TRUE), data_list_func)
+
+#generate a list of unique organisms 
+unique_org = unique(merged_data_org$protein)
+
+# Replace NA with 0 in the merged data frame
+merged_data_org[is.na(merged_data_org)] <- 0
+
+#Check if transformation was done correctly
+#sum of rel_abundance columns if 100
+columns_sum_org = colSums(merged_data_org[, -1], na.rm = TRUE)
+
+# Save the merged data to a new file (optional)
+fwrite(merged_data_org, "output_data/merged_data_org.tsv", sep = "\t")
+
+# Display the first few rows of the merged data
+head(merged_data_org)
+
+# Remove columns that start with "rel"
+merged_data_org <- merged_data_org %>%
+  select(-starts_with("rel"))
+
+# Remove the prefix "reads_" from column names
+names(merged_data_org) <- gsub("^reads_", "", names(merged_data_org))
+
+# Save the merged data to a new file (optional)
+fwrite(merged_data_org, "output_data/merged_data_org_reads.tsv", sep = "\t")
 
