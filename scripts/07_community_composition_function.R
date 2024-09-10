ccd776f475d17c1962d9f2bd5c1ac6201fb14666
AMahajna <asala.mahajna@gmail.com>
2024-09-10 17:41:13 +0200

    diversity plots function (heatmap)

--- scripts/07_community_composition_function.R
+++ scripts/07_community_composition_function.R
@@ -1,19 +1,27 @@
 ##Diversity heatmap 
 
-#set taxonomy ranks 
-colnames(rowData(tse_pathway))<- c("SEED_subsystem_functional_category", 
-                                   "description", 
-                                   "SEED_subsystem",
-                                   "enzyme")
-setTaxonomyRanks(colnames(rowData(tse_pathway)))
-ranks =getTaxonomyRanks()
-
-for (r in ranks) {
-  altExp(tse_pathway,r) <- agglomerateByRank(tse_pathway, r, agglomerate.tree = TRUE)
-}
+#Choose rank for plotting based in unique elements 
 #length(unique(rowData(tse_pathway)[ ,1]))
 
-tse_functional_category <- agglomerateByRank(tse_pathway,rank = "SEED_subsystem_functional_category")
+#Change rank names
+tse_pathway_right_rank <- tse_pathway
+colnames(rowData(tse_pathway_right_rank))<- c("SEED_subsystem_functional_category", 
+                                              "description", 
+                                              "SEED_subsystem",
+                                              "enzyme")
+setTaxonomyRanks(colnames(rowData(tse_pathway_right_rank)))
+ranks =c("SEED_subsystem_functional_category", 
+         "description", 
+         "SEED_subsystem",
+         "enzyme")
+
+for (r in ranks) {
+  altExp(tse_pathway_right_rank,r) <- agglomerateByRank(tse_pathway_right_rank, r, agglomerate.tree = TRUE)
+}
+
+################################################################################
+##Heatmap functional category
+tse_functional_category <- altExp(tse_pathway_right_rank, "SEED_subsystem_functional_category")
 
 # Add clr-transformation on samples
 tse_functional_category <- transformAssay(tse_functional_category, MARGIN = "samples", method = "clr", assay.type = "counts", pseudocount=1)
@@ -24,49 +32,40 @@ tse_functional_category <- transformAssay(tse_functional_category, assay.type = 
                                           method = "standardize", name = "clr_z")
 
 # Gets the assay table
-mat <- assay(tse_functional_category, "clr_z")
+mat_fun_cat <- assay(tse_functional_category, "clr_z")
 
 
 #png(filename="figures/heatmap_functional_category.png" ,units = 'in',width=9, height=6, res=1000)
 # Creates the heatmap
-pheatmap(mat)
+pheatmap(mat_fun_cat)
 #dev.off()
 
+################################################################################
+##Heatmap core enzymes 
 
-
-core_description = getPrevalentFeatures(altExp(tse_pathway,"description"), detection = 0, prevalence = 99/100,
-                                        rank = "description", sort = TRUE)
+#core_description = getPrevalentFeatures(altExp(tse_pathway_right_rank,"description"), detection = 0, prevalence = 99/100,
+#                                        rank = "description", sort = TRUE)
 
 #check
-sum((getPrevalence(altExp(tse_pathway,"description"), detection = 1/100, prevalence =1, 
-                   sort = TRUE, assay.type = "counts",
-                   as.relative = TRUE))== 1)
+#sum((getPrevalence(altExp(tse_pathway,"description"), detection = 1/100, prevalence =1, 
+#                   sort = TRUE, assay.type = "counts",
+#                   as.relative = TRUE))== 1)
 
 
-core_subsystem = getPrevalentFeatures(altExp(tse_pathway,"SEED_subsystem"), detection = 0, prevalence = 99/100,
-                                      rank = "SEED_subsystem", sort = TRUE)
+#core_subsystem = getPrevalentFeatures(altExp(tse_pathway,"SEED_subsystem"), detection = 0, prevalence = 99/100,
+#                                      rank = "SEED_subsystem", sort = TRUE)
 
 
-core_enzyme = getPrevalentFeatures(tse_pathway, detection = 0, prevalence = 0.999,
+core_enzyme = getPrevalentFeatures(tse_pathway_right_rank, detection = 0, prevalence = 0.999,
                                    rank = "enzyme", sort = TRUE)
 
-sum((getPrevalence(tse_pathway, tank ="enzyme", detection = 1/100, prevalence =1,  
+sum((getPrevalence(tse_pathway_right_rank, tank ="enzyme", detection = 1/100, prevalence =1,  
                    sort = TRUE, assay.type = "counts",
                    as.relative = TRUE))== 1)
 
-tse_enzyme <- agglomerateByRank(tse_pathway,rank = "enzyme")
+tse_enzyme <- agglomerateByRank(tse_pathway_right_rank,rank = "enzyme")
 
 tse_enzyme  <- transformAssay(tse_enzyme , method = "relabundance")
-
-# Add clr-transformation on samples
-tse_enzyme <- transformAssay(tse_enzyme, MARGIN = "samples", method = "clr", assay.type = "counts", pseudocount=1)
-
-# Add standardize-transformation on features (taxa)
-tse_enzyme <- transformAssay(tse_enzyme, assay.type = "clr",
-                             MARGIN = "features", 
-                             method = "standardize", name = "clr_z")
-
-
 
 tse_enzyme_subset <- tse_enzyme[core_enzyme, ]
 
@@ -81,15 +80,18 @@ tse_enzyme_subset  <- transformAssay(tse_enzyme_subset , assay.type = "clr",
 
 # Gets the assay table
 
-mat <- assay(tse_enzyme_subset, "clr_z")
+mat_core_enzyme <- assay(tse_enzyme_subset, "clr_z")
 
-png(filename="figures/heatmap_core_enzyme.png" ,units = 'in',width=9, height=6, res=1000)
+#png(filename="figures/heatmap_core_enzyme.png" ,units = 'in',width=9, height=6, res=1000)
 # Creates the heatmap
-pheatmap(mat)
-dev.off()
+pheatmap(mat_core_enzyme)
+#dev.off()
+
+################################################################################
+##Heatmap core enzymes clusters  
 
 # Hierarchical clustering
-taxa_hclust <- hclust(dist(mat), method = "complete")
+taxa_hclust <- hclust(dist(mat_core_enzyme), method = "complete")
 
 # Creates a phylogenetic tree
 taxa_tree <- as.phylo(taxa_hclust)
@@ -123,7 +125,7 @@ rowData(tse_enzyme_subset)$clusters <- taxa_clusters[order(match(rownames(taxa_c
 rowData(tse_enzyme_subset)$clusters
 
 # Hierarchical clustering
-sample_hclust <- hclust(dist(t(mat)), method = "complete")
+sample_hclust <- hclust(dist(t(mat_core_enzyme)), method = "complete")
 
 # Creates a phylogenetic tree
 sample_tree <- as.phylo(sample_hclust)
@@ -147,13 +149,11 @@ sample_data <- data.frame(clusters = sample_clusters)
 # Order data so that it's same as in phylo tree
 sample_data <- sample_data[samples_ordered, , drop = FALSE] 
 
-
-
 # Order data based on 
 tse_enzyme_subset <- tse_enzyme_subset[ , rownames(sample_data)]
 
 # Add sample type data
-sample_data$sample_types <- unfactor(colData(tse_enzyme_subset)$Season)
+#sample_data$sample_types <- unfactor(colData(tse_enzyme_subset)$Season)
 
 ############
 # Use left_join to merge df1 and df2 by the shared column 'Date'
@@ -165,14 +165,14 @@ sample_data <- sample_data %>%
 rownames(sample_data) = sample_data$Date
 sample_data$Date = NULL
 ############
-png(filename="figures/heatmap_core_enzyme_clusters.png" ,units = 'in',width=9, height=6, res=1000)
+#png(filename="figures/heatmap_core_enzyme_clusters.png" ,units = 'in',width=12, height=6, res=1000)
 
-breaks <- seq(-ceiling(max(abs(mat))), ceiling(max(abs(mat))), 
-              length.out = ifelse( max(abs(mat))>5, 2*ceiling(max(abs(mat))), 10 ) )
+breaks <- seq(-ceiling(max(abs(mat_core_enzyme))), ceiling(max(abs(mat_core_enzyme))), 
+              length.out = ifelse( max(abs(mat_core_enzyme))>5, 2*ceiling(max(abs(mat_core_enzyme))), 10 ) )
 colors <- colorRampPalette(c("darkblue", "blue", "white", "red", "darkred"))(length(breaks)-1)
 
-pheatmap(mat, annotation_row = taxa_clusters, 
+pheatmap(mat_core_enzyme, annotation_row = taxa_clusters, 
          annotation_col = sample_data,
          breaks = breaks,
          color = colors, border_color = grey)
-dev.off()
+#dev.off()
